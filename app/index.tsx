16ddf6457ed474bc7e9f99e73ed0148f0feca622
Pavithra Muniraju <pavithra.muniraju@ust.com>
2025-11-12 13:51:55 +0530

    example routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -5,6 +6,8 @@ export default function Index() {
     <View className="flex-1 justify-center items-center"
     >
       <Text className="text-4xl font-bold text-dark-200">Hello Pavi</Text>
+      <Link href="/onBoarding">Onboarding</Link>
+      <Link href="/movie/ave">movie</Link>
     </View>
   );
 }

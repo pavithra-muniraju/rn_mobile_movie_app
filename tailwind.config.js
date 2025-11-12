079e7e479206862be45b04aefc3b048304e9c5b3
Pavithra Muniraju <pavithra.muniraju@ust.com>
2025-11-12 12:01:44 +0530

    custom themes using tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: '#D6C6FF',
+          200: '#b0afb4ff'
+        },
+        dark: {
+          100: '#1E1E1E',
+          200: '#121212'
+        },
+        acent: "#AB8BFF"
+      }
+    },
   },
   plugins: [],
 }
